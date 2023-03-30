d53c3f0685998b47b4c329e94f8e63d97de6bffb
GonzaloNicolasPalavecino <128102847+GonzaloNicolasPalavecino@users.noreply.github.com>
2023-03-30 17:32:26 -0300

    Add files via upload

---
+++ Palindromo/js/f.js
@@ -0,0 +1,21 @@
+function verificarPalindromo() {
+      
+        
+        frase = frase.replace(/[\W_]/g, "").toLowerCase();
+
+        
+        let esPalindromo = true;
+        for (var i = 0; i < frase.length / 2; i++) {
+          if (frase[i] != frase[frase.length - i - 1]) {
+            esPalindromo = false;
+            break;
+          }
+        }
+
+        
+        if (esPalindromo) {
+          alert("SI");
+        } else {
+          alert("NO");
+        }
+      }
