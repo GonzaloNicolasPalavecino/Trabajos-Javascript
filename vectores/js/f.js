95fd4f2f4264478302869f89bea72b23deb19c23
GonzaloNicolasPalavecino <128102847+GonzaloNicolasPalavecino@users.noreply.github.com>
2023-06-22 17:17:56 -0300

    Add files via upload

--- vectores/js/f.js
+++ vectores/js/f.js
@@ -22,27 +22,11 @@ function Vectores() {
 
         vectorC.push(numeroAleatorio * 2);
       }
-		let mensaje1 = '';
-		mensaje = "Vector A: " + vectorA.join(", ") + "<br>";
-		return mensaje1;
-  
-		let mensaje2 = '';
-		mensaje2 = "Vector B: " + vectorB.join(", ") + "<br>";
-		return mensaje2;
-  
-		let mensaje3 = '';
-		mensaje3 =  "Vector C: " + vectorC.join(", ") + "<br>";
-		return mensaje3;
-	 
-		let mensaje4 = '';
-		mensaje4 = "Promedio del vector A: " + (suma / n).toFixed(2) + "<br>";
-		return mensaje4;
-	 
-		let mensaje5 = '';
-		mensaje5 = "Múltiplos de 2 y 3 en el vector A: " + multiplosDosYTres + "<br>";
-		return mensaje5;
-      
-		let mensaje6 = '';
-		mensaje6 = "Suma del vector A: " + suma + "<br>";
-		return mensaje6;
+		
+		console.log("Vector A: " + vectorA.join(", ") + "<br>");
+		console.log("Vector B: " + vectorB.join(", ") + "<br>");
+	        console.log("Vector C: " + vectorC.join(", ") + "<br>");
+		console.log("Promedio del vector A: " + (suma / n).toFixed(2) + "<br>");			        console.log("Multiplos de 2 y 3 del vector A: " + multiplosDosYTres + "<br>");	
+     		console.log("Suma del vector A: " + suma + "<br>");
+		
     }
