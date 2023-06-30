81fc2a2fdf2d6ed38769e062e6650881f473084a
GonzaloNicolasPalavecino <128102847+GonzaloNicolasPalavecino@users.noreply.github.com>
2023-06-30 18:34:54 -0300

    Update class.js

--- Js Class 02/js/class.js
+++ Js Class 02/js/class.js
@@ -1,12 +1,33 @@
 class CEmpleado {
   constructor(nombre, antiguedad, direccion) {
     this.nombre = nombre;
-    this.antiguedad = antiguedad;
-    this.direccion = direccion;
+    this.antiguedad = 2014;
+    this.direccion = "64C- WallStreet";
   }
+  AñosdeServicio(){
+  var Antiguedad = new date();
+    var año actual = actual.getFullYear();
+    var diferencia = actual.getFullYear() - Antiguedad.getFullYear();
+
+    return diferencia
+  }
+
+  viveEn(direccion){
+    if(this.direccion = "64C- WallStreet"){
+     return "Vive en WallStreet";
+    }else {
+     return "No vive en WallStreet";
+    }
+  }
+  
 }
 
-const empleado1 = new CEmpleado("Robert", 2014, "64C- WallStreet");
-document.write("Nombre: " + empleado1.nombre + "<br>");
-document.write("Años de antiguedad: " + empleado1.antiguedad + "<br>");
-document.write("Direccion: " + empleado1.direccion + "<br>");
+const empleado1 = new CEmpleado(nombre, antiguedad, direccion);
+const localidad = empleado1.ViveEn();
+const YearsOld = empleado1.AñosdeServicio()
+
+const mostrarDatos = `Nombre: ${nombre} <br> Localidad: ${direccion} <br> ${localidad} <br> Cantidad de años en servicio: ${YearsOld} <br> Esta en la comanpania desde : ${direccion} `;
+
+
+
+document.write(mostrarDatos);
