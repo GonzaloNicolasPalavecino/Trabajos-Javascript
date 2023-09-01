49ae356014c0846f056cae9ccf38db57ec397f9c
GonzaloNicolasPalavecino <128102847+GonzaloNicolasPalavecino@users.noreply.github.com>
2023-09-01 19:14:42 -0300

    Add files via upload

---
+++ ppt/app.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('game-form');
+const userChoiceInput = document.getElementById('user-choice');
+const resultText = document.getElementById('result');
+
+const options = ['piedra', 'papel', 'tijera'];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+   
+    const userChoice = userChoiceInput.value.toLowerCase();
+
+   
+    const computerChoice = options[Math.floor(Math.random() * 3)];
+
+  
+    resultText.innerHTML = `<br> Elegiste: ${userChoice}<br>La computadora eligio: ${computerChoice}<br>${determineWinner(userChoice, computerChoice)}`;
+});
+
+function determineWinner(user, computer) {
+    if (user === computer) {
+        return "Empate :/";
+    } else if ((user === 'piedra' && computer === 'tijera') || (user === 'papel' && computer === 'piedra') || (user === 'tijera' && computer === 'papel')) {
+        return "Ganaste :D";
+    } else {
+        return "Perdiste :(";
+    }
+}
