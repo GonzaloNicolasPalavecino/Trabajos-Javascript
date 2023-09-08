a2c4c30b7b683207d96e631561cbc0d1927faffc
GonzaloNicolasPalavecino <128102847+GonzaloNicolasPalavecino@users.noreply.github.com>
2023-09-08 19:31:30 -0300

    Delete mysql directory

--- mysql/17/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static(__dirname + '/public'))
-
-const server = app.listen(8888, () => {
-    console.log('Servidor web iniciado')
-})
