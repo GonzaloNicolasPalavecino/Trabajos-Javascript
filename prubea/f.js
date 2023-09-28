2a75204451c367b677498736ad889aa0157a9e61
GonzaloNicolasPalavecino <128102847+GonzaloNicolasPalavecino@users.noreply.github.com>
2023-09-28 18:40:31 -0300

    Update f.js

--- prubea/f.js
+++ prubea/f.js
@@ -1,3 +1,30 @@
+const form = document.getElementById('traductorForm');
+const resultadoElement = document.getElementById('resultado');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const frase = document.getElementById('frase').value;
+
+    try {
+        const response = await fetch('/traducir', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ frase }),
+        });
+
+        if (response.ok) {
+            const traduccion = await response.text();
+            resultadoElement.textContent = traduccion;
+        } else {
+            resultadoElement.textContent = 'Error en la traducción.';
+        }
+    } catch (error) {
+        resultadoElement.textContent = 'Error en la solicitud.';
+    }
+});
+
 function traducirFrase() {
     const frase = document.getElementById('frase').value;
     const resultadoElement = document.getElementById('resultado');
