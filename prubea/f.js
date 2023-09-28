7a83b47a8da1db7c693c46bb7b5e4dd34bb47e93
GonzaloNicolasPalavecino <128102847+GonzaloNicolasPalavecino@users.noreply.github.com>
2023-09-28 18:47:10 -0300

    Update f.js

--- prubea/f.js
+++ prubea/f.js
@@ -1,29 +1,33 @@
-const form = document.getElementById('traductorForm');
-const resultadoElement = document.getElementById('resultado');
+const http = require('http');
+const fs = require('fs');
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const frase = document.getElementById('frase').value;
+const servidor = http.createServer((envio, mostrar) => {
+  if (envio.method == 'GET' && envio.url == '/') {
+    mostrar.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    fs.createReadStream('index.html').pipe(mostrar);
+  } else if (envio.method == 'GET' && envio.url == '/f.js') {
+    mostrar.writeHead(200, { 'Content-Type': 'text/javascript; charset=utf-8' });
+    fs.createReadStream('f.js').pipe(mostrar);
+  } else if (envio.method == 'POST' && envio.url == '/traducir') {
+    let body = '';
 
-    try {
-        const response = await fetch('/traducir', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ frase }),
-        });
+    envio.on('data', (data) => {
+      body += data;
+    });
 
-        if (response.ok) {
-            const traduccion = await response.text();
-            resultadoElement.textContent = traduccion;
-        } else {
-            resultadoElement.textContent = 'Error en la traducción.';
-        }
-    } catch (error) {
-        resultadoElement.textContent = 'Error en la solicitud.';
-    }
+    envio.on('end', () => {
+      const mensajesv = traducirFrase();
+
+      mostrar.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+      mostrar.end(mensajesv);
+    });
+  }
 });
+
+servidor.listen(8888, () => {
+  console.log('Servidor web iniciado en el puerto 8888');
+});
+
 
 function traducirFrase() {
     const frase = document.getElementById('frase').value;
@@ -33,3 +37,4 @@ function traducirFrase() {
     
     resultadoElement.textContent = fraseTraducida;
 }
+
